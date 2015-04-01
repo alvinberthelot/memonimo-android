d1c7dfe010f8404268cb584f4db4405e26f80a3e
Alvin BERTHELOT <alvin.berthelot@gmail.com>
2015-04-01 14:26:49 +0200

    Création d'une activité vide pour commencer le jeu

--- app/src/main/java/com/webyousoon/android/memonimo/StartActivity.java
+++ app/src/main/java/com/webyousoon/android/memonimo/StartActivity.java
@@ -1,9 +1,11 @@
 package com.webyousoon.android.memonimo;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class StartActivity extends ActionBarActivity {
@@ -35,5 +37,10 @@ public class StartActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void startGame(View view) {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
     }
 }
